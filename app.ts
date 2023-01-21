41c970a4b4144fc00f9d0c673be73689881e6435
Bora <mail@borasoylu.com>
2023-01-21 17:50:15 +0300

    cors hotfix please don't break anything

--- app.ts
+++ app.ts
@@ -2,14 +2,21 @@ console.log('********* SERVER RESTARTED *********');
 import express, { Express, Request, Response } from 'express';
 import { router } from './src/controllers';
 import helmet from 'helmet';
-
 const app: Express = express();
+import cors from 'cors';
 
 app.use(express.json());
 app.use('/api/v1', router);
 app.use(helmet());
 const port = process.env.PORT;
-
+app.use(
+    cors({
+        origin: '*',
+    })
+);
+// app.use(cors({
+//     origin: 'https://www.farazywallet.com'
+// }))
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
